939b3cede8fa61a58b10317938126763b0154790
Ket3r <github@peter-kempter.de>
2020-03-11 21:40:04 +0100

    merged

--- transient/history.el
+++ transient/history.el
@@ -1,2 +1,16 @@
-((magit-commit nil)
- (magit-dispatch nil))
+((magit-branch nil)
+ (magit-commit nil)
+ (magit-diff
+  (("--" "kickstart/2019/kickstart_alarm/test/kickstart_alarm_group_runner.c"))
+  (("--" "kickstart/2019/kickstart_alarm/test/kickstart_alarm_group.c"))
+  (("--" "kickstart/2019/kickstart_alarm/src/kickstart_alarm.c")))
+ (magit-dispatch nil)
+ (magit-fetch nil)
+ (magit-gitignore nil)
+ (magit-log
+  ("-n256" "--graph" "--decorate"))
+ (magit-merge nil)
+ (magit-pull nil)
+ (magit-push nil)
+ (magit-submodule nil
+                  ("--recursive")))

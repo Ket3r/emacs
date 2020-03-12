0a3777b7b206c6b6eb066daebac6c007434d1411
Ket3r <github@peter-kempter.de>
2020-03-12 06:55:46 +0100

    installed elpy

---
+++ elpa/company-fuzzy-0.5.3/company-fuzzy-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/naboo/.emacs.d/elpa/company-fuzzy-0.5.3/company-fuzzy.el  -*- no-byte-compile: t -*-
+(define-package "company-fuzzy" "0.5.3" "Fuzzy matching for `company-mode'." '((emacs "24.4") (company "0.8.12") (s "1.12.0")) :commit "a97f55b60f427e536e637898d12792154d134aab" :authors '(("Shen, Jen-Chieh" . "jcs090218@gmail.com")) :maintainer '("Shen, Jen-Chieh" . "jcs090218@gmail.com") :url "https://github.com/jcs090218/company-fuzzy")
